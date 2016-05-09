6cb937830f99f46d9695fe7be3872bf2d266800c
Henrik Bolding Frank <henrik@vonfrank.dk>
2016-05-09 09:09:20 +0200

    Die class created (initial)

---
+++ app/src/main/java/group3/meyer_android/model/Die.java
@@ -0,0 +1,23 @@
+package group3.meyer_android.model;
+
+/**
+ * Created by Von Frank on 09-05-2016.
+ */
+public class Die {
+
+    private int face;
+
+    public Die(){
+
+    }
+
+    public int shuffel(){
+
+        return 0;
+    }
+
+    public int getDie(){
+
+        return 0;
+    }
+}
