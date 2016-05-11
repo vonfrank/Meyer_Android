eed393b62d90e58d99ea588c069e22f0b3c839e1
Deothan <mads@deothan.dk>
2016-05-11 13:46:30 +0200

    added buffered reader and writer

--- app/src/main/java/group3/meyer_android/controller/JoinActivity.java
+++ app/src/main/java/group3/meyer_android/controller/JoinActivity.java
@@ -8,8 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.UUID;
 
 import group3.meyer_android.R;
@@ -22,6 +27,9 @@ public class JoinActivity extends AppCompatActivity {
     private BluetoothSocket btSocket = null;
     private BluetoothAdapter btAdapter = null;
     private BluetoothDevice btDevice = null;
+    private BufferedReader bufferedreader;
+    private BufferedWriter bufferedwritter;
+    private String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,22 @@ public class JoinActivity extends AppCompatActivity {
 
         serverMac = getIntent().getStringExtra("mac");
         new SocketConnection(serverMac).execute();
+    }
+
+    private void makeInOutStreams(){
+        InputStream inputstream = null;
+        OutputStream outputStream = null;
+
+        try {
+            inputstream = btSocket.getInputStream();
+            outputStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+             e.printStackTrace();
+            return;
+        }
+
+        bufferedreader = new BufferedReader(new InputStreamReader(inputstream));
+        bufferedwritter = new BufferedWriter(new OutputStreamWriter(outputStream));
     }
 
     public void nextBtnClick(View view) {
@@ -52,7 +76,64 @@ public class JoinActivity extends AppCompatActivity {
         gf.hideBtnClick();
     }
 
-    private class SocketConnection extends AsyncTask<String, Void, BluetoothSocket> {
+    /**
+     * Constructor takes the Buffered writer used to write.
+     * The Execute takes the String to send.
+     */
+    private class SendText extends AsyncTask<String, Void, Void> {
+        private BufferedWriter bufferedwriter = null;
+
+        public SendText(BufferedWriter writer){
+            bufferedwriter = writer;
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                bufferedwriter.write(params[0]);
+                bufferedwriter.newLine();
+                bufferedwriter.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    /**
+     * Constructor takes the Buffered reader used to read.
+     * The class sets the data variable with the result from the read.
+     */
+    private class RecieveText extends AsyncTask<Void, Void, String> {
+        private BufferedReader bufferedreader = null;
+
+        public RecieveText(BufferedReader reader){
+            bufferedreader = reader;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String result = null;
+
+            try {
+                result = bufferedreader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            data = s;
+        }
+    }
+
+    /**
+     * Constructor takes the server mac address as a parameter.
+     * Returns the socket when there is s connection.
+     */
+    private class SocketConnection extends AsyncTask<Void, Void, BluetoothSocket> {
 
         private UUID RFCOMM_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
         private BluetoothAdapter mBluetoothAdapter;
@@ -71,7 +152,7 @@ public class JoinActivity extends AppCompatActivity {
         }
 
         @Override
-        protected BluetoothSocket  doInBackground(String... params) {
+        protected BluetoothSocket  doInBackground(Void... params) {
             mBluetoothAdapter.cancelDiscovery();
 
             try {
@@ -91,6 +172,7 @@ public class JoinActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(BluetoothSocket socket) {
             btSocket = socket;
+            makeInOutStreams();
         }
     }
 }
